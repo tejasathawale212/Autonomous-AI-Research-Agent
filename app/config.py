277e6fdf60684ce705f463fb424d94753cb376b5
tejasathawale212 <tejasathawale212@gmail.com>
2026-09-14 09:39:50 +0530

    Add environment configuration

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+QDRANT_URL = os.getenv("QDRANT_URL")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+
